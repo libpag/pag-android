7b2b3c91e7fb5508f53958e57415ef1055f4a2cc
kevingpqi <kevingpqi@tencent.com>
2022-02-10 12:58:17 +0800

    Update demo

--- sample/app/src/main/java/com/tencent/libpag/sample/libpag_sample/MainActivity.java
+++ sample/app/src/main/java/com/tencent/libpag/sample/libpag_sample/MainActivity.java
@@ -30,11 +30,11 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity implements SimpleListAdapter.ItemClickListener {
 
     private static final String[] items = new String[]{
-            "基础使用",
-            "替换文字",
-            "替换图片",
-            "多个PAGFile在同一个Surface中使用",
-            "纹理ID创建PAGSurface"
+            "A Simple PAG Animation",
+            "Text Replacement",
+            "Image Replacement",
+            "Render Multiple PAG Files on A PAGSurface",
+            "Create PAGSurface through texture ID"
     };
 
     @Override
